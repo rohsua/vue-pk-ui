f35702db88931314b278acf570ced8bc6ecd8116
mikwon_roh <yearn45@gmail.com>
2020-02-06 16:50:35 +0900

    feature: 상단탭 커스텀

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,27 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <v-tabs>
+      <v-tab>메뉴명1</v-tab>
+      <v-tab>메뉴명2</v-tab>
+      <v-tab>메뉴명3</v-tab>
+    </v-tabs>
     <v-content>
       <HelloWorld />
     </v-content>
   </v-app>
 </template>
+
+<style lang="scss">
+// vuetify import
+// global sass import 로 변경 예정
+.v-tabs {
+  background: #f4f4f4;
+
+  .v-tabs-slider {
+    display: none;
+  }
+}
+</style>
 
 <script lang="ts">
 import Vue from "vue";
