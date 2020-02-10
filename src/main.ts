3c7d7c9b2bec70530d677caaedc8dd9ee562f620
mikwon_roh <yearn45@gmail.com>
2020-02-10 15:37:45 +0900

    refactor: vuetify preset 적용으로 vue 파일 우선순위 삭제

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import router from "./router";
 import store from "./store";
+import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import App from "./App.vue"; // vuetify보다 하위에 있어야 함.
 
 Vue.config.productionTip = false;
 
