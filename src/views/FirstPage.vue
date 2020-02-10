633112c1afb2dfe3b4fd1fc5fc9e50db4cf0fd0b
mikwon_roh <yearn45@gmail.com>
2020-02-10 17:51:14 +0900

    feature: carousel 추가

--- src/views/FirstPage.vue
+++ src/views/FirstPage.vue
@@ -1,18 +1,103 @@
 <template>
-  <div class="first-page">
+  <div class="wrap">
     <Tabs />
-    첫번째 페이지
+    <carousel
+      :per-page="1"
+      :mouse-drag="true"
+      :paginationEnabled="false"
+      :adjustableHeight="true"
+    >
+      <slide>
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+        Slide 1 Content <br />
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+      <slide>
+        Slide 3 Content
+      </slide>
+    </carousel>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+// @ts-ignore
+import { Carousel, Slide } from "vue-carousel";
 import Tabs from "@/components/Tabs.vue";
 
 export default Vue.extend({
   name: "FirstPage",
   components: {
-    Tabs
+    Tabs,
+    Carousel,
+    Slide
   }
 });
 </script>
