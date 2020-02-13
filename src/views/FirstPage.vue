448cd989deb0858cb32f3a91d2abadbab9829dc5
mikwon_roh <yearn45@gmail.com>
2020-02-13 16:12:17 +0900

    feature: chartjs 추가

--- src/views/FirstPage.vue
+++ src/views/FirstPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrap">
-    <Tabs :currentSlide="currentSlide" @change:currentTab="handleTabChange" />
+    <tabs :currentSlide="currentSlide" @change:currentTab="handleTabChange" />
     <carousel
       :value="currentSlide"
       :per-page="1"
@@ -10,79 +10,13 @@
       @page-change="handleSlideChange"
     >
       <slide>
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
-        Slide 1 Content <br />
+        Slide 1 Content
       </slide>
       <slide>
         Slide 2 Content
       </slide>
       <slide>
-        Slide 3 Content
+        <bar-chart :chart-data="chartdata" :options="options"></bar-chart>
       </slide>
     </carousel>
   </div>
@@ -93,17 +27,71 @@ import Vue from "vue";
 // @ts-ignore
 import { Carousel, Slide } from "vue-carousel";
 import Tabs from "@/components/Tabs.vue";
+// @ts-ignore
+import BarChart from "@/components/Chart.vue";
+
+let chartColors = {
+  red: "rgb(255, 99, 132)",
+  orange: "rgb(255, 159, 64)",
+  yellow: "rgb(255, 205, 86)",
+  green: "rgb(75, 192, 192)",
+  blue: "rgb(54, 162, 235)",
+  purple: "rgb(153, 102, 255)",
+  grey: "rgb(201, 203, 207)"
+};
+const chartdata = {
+  labels: ["1월", "2월", "3월"],
+  datasets: [
+    {
+      label: "Dataset 1",
+      backgroundColor: chartColors.red,
+      data: [2, 4, 35, 23]
+    },
+    {
+      label: "Dataset 2",
+      backgroundColor: chartColors.blue,
+      data: [4, 99, 28, 97]
+    }
+  ]
+};
+
+const options = {
+  title: {
+    display: true,
+    text: "Chart.js Bar Chart - Stacked"
+  },
+  tooltips: {
+    mode: "index",
+    intersect: false
+  },
+  responsive: true,
+  scales: {
+    xAxes: [
+      {
+        stacked: true
+      }
+    ],
+    yAxes: [
+      {
+        stacked: true
+      }
+    ]
+  }
+};
 
 export default Vue.extend({
   name: "FirstPage",
   components: {
     Tabs,
     Carousel,
-    Slide
+    Slide,
+    BarChart
   },
   data() {
     return {
-      currentSlide: 0
+      currentSlide: 0,
+      chartdata: {},
+      options: {}
     };
   },
   methods: {
