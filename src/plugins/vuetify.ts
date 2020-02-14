4c4fece3c3f756505c138d16fcb7ca37e509ccac
mikwon_roh <yearn45@gmail.com>
2020-02-14 11:23:16 +0900

    refactor: shrine preset 삭제

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-// import { preset } from "vue-cli-plugin-vuetify-preset-shrine/preset";
 import preset from "./preset/preset";
 
 Vue.use(Vuetify);
